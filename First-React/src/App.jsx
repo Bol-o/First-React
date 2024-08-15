8747d92a02c79fdcf043bb47cea6e3a6828e5090
Bolo New <121732191+Bol-o@users.noreply.github.com>
2024-08-15 12:53:20 +0100

    modified to hiya worldo

--- First-React/src/App.jsx
+++ First-React/src/App.jsx
@@ -4,30 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(10)
+  // const [count2, setCount2] = useState(11)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      {/*  */}
+      <h1>VVitek + Relaxs & hiya worldo</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => setCount2((count2) => count2 + 2)}>
+          count is {count2}
+        </button> 
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
