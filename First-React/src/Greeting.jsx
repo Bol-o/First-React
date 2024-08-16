4186d10b5f6857d2abb3e9c8fdab3ac427baf154
Bolo New <121732191+Bol-o@users.noreply.github.com>
2024-08-16 08:08:20 +0100

    trying out different export commands

---
+++ First-React/src/Greeting.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+// import './index.css'
+
+
+ function Greeting() {
+    return <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>;
+  }
+  
+
+  export default Greeting
