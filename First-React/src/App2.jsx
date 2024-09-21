cf73f15568483dbccb2a61f9afca15dac3811641
Bolo New <121732191+Bol-o@users.noreply.github.com>
2024-09-21 07:57:07 +0100

    setState fucntion in useState can be skipped
    And you can use just the variable as an array, and then use .push to add
    anything to lenghten the array, then when displaying result in DOM just
    use .length. Not sure if this helps with anything as it work well with
    setState fucntions, just messing around

--- First-React/src/App2.jsx
+++ First-React/src/App2.jsx
@@ -1,46 +1,68 @@
 import React, { useState } from "react";
 import "./App2.css";
+import CustomInput from "../public/CustomInput";
+
 
 const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 
- export function App() {
+  function App() {
+    const [value, setValue] = useState("");
+    const [value2, setValue2] = useState("");
+
+  const [person, setPerson] = useState({name: 'John', age: 100})  
   
   const [colorArr] = useState([])
 
-  const[count, setCount] = useState(0);
+  const[count] = useState([]);
+  // alert("b4 onclik: " + count)
   
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
 
-  const onButtonClick = (color, count) => () => {
-   
+  const onButtonClick = (color) => () => {
+    alert("AFTA onclik: " + count)
     //alert(colorArr)
     
     setBackgroundColor(color);
     
     colorArr.push(color);
+  
 
-    // alert('whole thing:  ' + colorArr)
+    // alert('count before ifs:  ' + count)
+    // alert('colorArr before ifs:  ' + colorArr)
     // alert('whole thing:  ' + colorArr)
     // alert(colorArr[colorArr.length - 1])
     // alert('arr length is:  ' + colorArr.length)
 
     
     
-    if (colorArr.length == 1 && colorArr[0] !== COLORS[0]){
-        count++
+    if (colorArr.length == 1 && colorArr[0] == COLORS[0]){
+      alert('counter not going up cause same color: ' + colorArr[colorArr.length - 1])
+    } else if (colorArr.length == 1 && colorArr[0] !== COLORS[0]){
+      // alert('it is not he pink and count b4 ++  is: ' + count)
+        count.push(1)
+        // count = count.length
+        // alert('it is not he pink and count is: '+ count)
 
       }else if (colorArr.length > 1 && color == colorArr[colorArr.length - 2]){
         alert('counter not going up cause same color: ' + colorArr[colorArr.length - 1])
+        
       } else if (colorArr.length > 1 && color !== colorArr[colorArr.length - 2]){
-        count++
+        count.push(1)
+        // count = count.length
+        // alert('it is not he pink and count is: ' + count)
       }
     
 
-    
+      // alert('count After ifs:  ' + count)
+      // alert('clourArr After ifs:  ' + colorArr)
 
     
     
-    setCount(count)
+    count++
+
+    setPerson((person) => ({ ...person, age: person.age + 1 }));
+    
+    setPerson((person) => ({ ...person, age: person.age + 1 }));
     
 
   };
@@ -63,7 +85,7 @@ const COLORS = ["pink", "green", "blue", "yellow", "purple"];
         <button
           type="button"
           key={color}
-          onClick={onButtonClick(color, count)}
+          onClick={onButtonClick(color)}
          
           className={backgroundColor === color ? "selected" : ""}
         >
@@ -73,24 +95,30 @@ const COLORS = ["pink", "green", "blue", "yellow", "purple"];
       ))}
      
     </div>
-    <label className="CountboxLabel"  for="CountBox">Count:</label>
-    <div className="CountBox">
-      {count}
-    </div>
-    </>
-  );
-}
-
-export function CustomInput() {
-  const [value, setValue] = useState("");
-
-  return (
-    <input
-      className="CustomInput"
+    <div>
+      <h1>{person.name} : {person.age}</h1>
+      <h1><input
       type="text"
       value={value}
       onChange={(event) => setValue(event.target.value)}
     />
+    <input
+      type="text"
+      value={value2}
+      onChange={(event) => setValue2(event.target.value)}
+    /></h1>
+    </div>
+    <label className="CountboxLabel"  for="CountBox">Count:</label>
+    <div className="CountBox">
+      {count.length
+      }
+      
+    </div>
+    <div className="CustomInput"><CustomInput /></div>
+    
+    </>
   );
 }
+
+export default App
 
--- First-React/src/main2.jsx
+++ First-React/src/main2.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {App, CustomInput} from './App2';
+import App from './App2';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <CustomInput />
     
   </React.StrictMode>,
 );
